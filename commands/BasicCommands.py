194e3467a574f59ef246f2694a4124cbc8bec9c7
Vic <125327027+PandArtGit@users.noreply.github.com>
2023-03-03 08:10:29 -0300

    Update 0.3

--- commands/BasicCommands.py
+++ commands/BasicCommands.py
@@ -2,42 +2,72 @@ import discord
 from objectClass import Iniciativa
 from discord.ext import commands
 
-objeto = Iniciativa()
 
 class BasicCommands(commands.Cog):
-    
-    
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        super().__init__()
-    
-    
-    
-    @discord.slash_command(name="ajuda")
-    async def Test(self, ctx: commands.context):
+        self.objeto = Iniciativa()
+        
+    @commands.slash_command(name="mostrar")
+    async def Mostrar(self, ctx: commands.Context):
+        iniciativas = self.objeto.iniciativas.get(ctx.channel.id, [])
+        if not iniciativas:
+            mensagem = "Não há iniciativas nesse chat."
+        else:
+            mensagem = "\n".join("{:2d}       {:<10}       [{:2d}]".format(i, nome, iniciativa) for i, (nome, iniciativa) in enumerate(iniciativas))
+        e = discord.Embed(title="INICIATIVA")
+        e.add_field(name="Iniciativas", value=mensagem, inline=True)
+        await ctx.respond(embed=e)
+        
+    @commands.slash_command(name="ajuda")
+    async def Ajuda(self, ctx: commands.Context):
         e = discord.Embed(title="COMANDOS", description='''
                           /adicionar <nome> <iniciativa>
-                          
                           /mostrar
-                          
+                          /matar <ID ou NOME>
                           ''')
-        await ctx.respond(embed = e)
-    
-    @discord.slash_command(name="adicionar")
-    async def Adicionar(self, ctx: commands.context, nome: str, iniciativa: int):
-        # chamar a função de adicionar
-        objeto.adicionar(nome, iniciativa)
-        #-----------------------------
+        await ctx.respond(embed=e)
+
+    @commands.slash_command(name="nova_iniciativa")
+    async def NovaIniciativa(self, ctx: commands.Context):
+        self.objeto.iniciativas[ctx.channel.id] = []
+        await self.Mostrar(ctx)
+        await ctx.respond("Nova iniciativa criada.")
+
+    @commands.slash_command(name="adicionar")
+    async def Adicionar(self, ctx: commands.Context, nome: str, iniciativa: int):
+        self.objeto.adicionar_iniciativa_chat(ctx.channel.id, nome, iniciativa)
+        await self.Mostrar(ctx)
         await ctx.respond("Adicionado")
     
-    @discord.slash_command(name="mostrar")
-    async def Mostrar(self, ctx: commands.context):
-        # chamar a função de mostrar
-        mensagem = objeto.mostrar()
-        #-----------------------------
-        e = discord.Embed(title="INICIATIVA", description=mensagem)
-        await ctx.respond(embed = e)
-        
+    @discord.slash_command(name="matar")
+    async def Matar(self, ctx: commands.context, referencia: str):
+        chat_id = ctx.channel.id
+        iniciativas = self.objeto.iniciativas.get(chat_id, [])
+            
+        if referencia.isnumeric():
+            # Se a referência for um número, consideramos que é o índice da iniciativa na lista
+            index = int(referencia)
+            if index < 0 or index >= len(iniciativas):
+                await self.Mostrar(ctx)
+                await ctx.respond("Índice inválido.")
+                return
+            del iniciativas[index]
+        else:
+            # Se a referência for uma string, consideramos que é o nome da iniciativa
+            # Procuramos pelo nome na lista de iniciativas
+            for i, (nome, _) in enumerate(iniciativas):
+                if nome.lower() == referencia.lower():
+                    del iniciativas[i]
+                    break
+            else:
+                await self.Mostrar(ctx)
+                await ctx.respond("Nome inválido.")
+                return
+            
+        self.objeto.iniciativas[chat_id] = sorted(iniciativas, key=lambda x: x[1]) # ordena a lista pelo valor da iniciativa
+        await self.Mostrar(ctx)
+        await ctx.respond("Iniciativa deletada com sucesso.")
+
 def setup(bot: commands.Bot):
     bot.add_cog(BasicCommands(bot))
-    
--- objectClass.py
+++ objectClass.py
@@ -1,17 +1,24 @@
-
-
-
 class Iniciativa:
     def __init__(self):
-        self.list_nome = []
-        self.list_iniciativa = []
-    
+        self.iniciativas = {}
+
     def adicionar(self, _nome, _iniciativa):
         self.list_nome.append(_nome)
         self.list_iniciativa.append(_iniciativa)
-    
-    def mostrar(self):
+
+    def mostrar(self, chat_id):
         mensagem = ""
-        for x in range(len(self.list_nome)):
-            mensagem += f"ID: {x} - {self.list_nome[x]} - INICIATIVA: {self.list_iniciativa[x]}\n"     
+        iniciativas = sorted(self.iniciativas.get(chat_id, []), key=lambda x: x[1], reverse=True)
+        for i, (nome, iniciativa) in enumerate(iniciativas):
+            mensagem += "{:2d}  {:<10}  [{:2d}]\n".format(i, nome, iniciativa)
+        if not mensagem:
+            mensagem = "Não há iniciativas nesse chat."
         return mensagem
+
+    def adicionar_iniciativa_chat(self, chat_id, nome, iniciativa):
+        if chat_id not in self.iniciativas:
+            self.iniciativas[chat_id] = []
+        self.iniciativas[chat_id].append((nome, iniciativa))
+        self.iniciativas[chat_id] = sorted(self.iniciativas[chat_id], key=lambda x: x[1])
+        
+    
